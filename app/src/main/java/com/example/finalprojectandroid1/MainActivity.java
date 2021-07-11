28d3c5e0560718719ed83f4206a6927c4015a92c
OhadBokin <hotwinter360@gmail.com>
2021-07-11 16:09:06 +0300

    game works updated 8

--- app/src/main/java/com/example/finalprojectandroid1/MainActivity.java
+++ app/src/main/java/com/example/finalprojectandroid1/MainActivity.java
@@ -1,36 +1,32 @@
 package com.example.finalprojectandroid1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button play;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.acitivity_main_screen);
+
 
         play = (Button) findViewById(R.id.playBtn);
-        play.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               openArcadePong();
+            play.setOnClickListener((new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  Intent  intent = new Intent(MainActivity.this, ArcadePong.class);
+                  startActivity(intent);
             }
-        }));
-    }
-    public void openArcadePong(){
-        Intent  intent = new Intent(this,ArcadePong.class);
-        startActivity(intent);
-    }
+             }));
+   }
 
 }
