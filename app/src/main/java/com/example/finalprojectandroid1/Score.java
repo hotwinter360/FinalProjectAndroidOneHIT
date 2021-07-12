f3583667585a5d67d2422e7e0a83eea030ee25c5
OhadBokin <hotwinter360@gmail.com>
2021-07-13 01:11:49 +0300

    game working updated 16 Return to main activity from score board

--- app/src/main/java/com/example/finalprojectandroid1/Score.java
+++ app/src/main/java/com/example/finalprojectandroid1/Score.java
@@ -26,6 +26,12 @@ public class Score extends Activity {
 
         tv_score = (TextView) findViewById(R.id.tv_score);
         EditText nameEt1 = findViewById(R.id.Name_input);
+        String alert_msg_no_name = getResources().getString(R.string.alert_msg_no_name_entered);
+        String alert_title_no_name = getResources().getString(R.string.alert_title_no_name_entered);
+        String Best1 = getResources().getString(R.string.Best1);
+        String Best2 = getResources().getString(R.string.Best2);
+        String Best3 = getResources().getString(R.string.Best3);
+        String lastscore = getResources().getString(R.string.lastscore);
         int best1,best2,best3;
 
         Button addBtn = findViewById(R.id.Add);
@@ -33,25 +39,29 @@ public class Score extends Activity {
         SharedPreferences sp = getSharedPreferences("Score",MODE_PRIVATE);
 
         int LastScore = sp.getInt("Score",0);
-        best1 = sp.getInt("best1",0);
-        best2 = sp.getInt("best2",0);
-        best3 = sp.getInt("best3",0);
+        best1 = sp.getInt("Best1",0);
+        best2 = sp.getInt("Best2",0);
+        best3 = sp.getInt("Best3",0);
 
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String name = nameEt1.getText().toString();
-                if (nameEt1.equals("")) {
+                String name = tv_score.getText().toString();
+                if ((name.equals("") || name.matches("[0-9]+"))) {
                     AlertDialog.Builder alertDialog = new AlertDialog.Builder(Score.this);
-                    alertDialog.setTitle("alert_msg_no_number");
-                    alertDialog.setMessage("alert_title_no_number_entered");
+                    alertDialog.setTitle("alert_msg_no_name");
+                    alertDialog.setMessage("alert_title_no_name_entered");
                     alertDialog.setPositiveButton("OK", null);
                     AlertDialog dialog = alertDialog.create();
                     dialog.show();
                 } else {
                 Toast.makeText(Score.this, (String) name, Toast.LENGTH_SHORT).show();
                 }
+                Animation animation = AnimationUtils.loadAnimation(Score.this, R.anim.bounce);
+                MyBounce interpolator = new MyBounce(0.2,20);
+                animation.setInterpolator(interpolator);
+                addBtn.startAnimation(animation);
 
             }
         });
@@ -79,11 +89,12 @@ public class Score extends Activity {
             editor.putInt("Best1",best1);
             editor.apply();
         }
-        tv_score.setText("Last Score: " + LastScore + "\n" + "Best1: " + best1 + "\n" + "Best2: " + best2 + "\n" + "Best3: " + best3 + "\n");
+        tv_score.setText((String) lastscore + ": " + LastScore + " " + "\n" + (String) Best1 + ": " + best1 + " " + "\n" + (String) Best2 + ": " + best2  + " " + "\n" + (String) Best3 + ": " + best3  + " " + "\n");
         playagainbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (nameEt1.equals("")) {
+                String name = tv_score.getText().toString();
+                if ((name.equals("") || name.matches("[0-9]+"))) {
                     AlertDialog.Builder alertDialog = new AlertDialog.Builder(Score.this);
                     alertDialog.setTitle("alert_msg_no_number");
                     alertDialog.setMessage("alert_title_no_number_entered");
@@ -95,6 +106,10 @@ public class Score extends Activity {
                     startActivity(intent);
                     finish();
                 }
+                Animation animation = AnimationUtils.loadAnimation(Score.this, R.anim.bounce);
+                MyBounce interpolator = new MyBounce(0.2,20);
+                animation.setInterpolator(interpolator);
+                playagainbtn.startAnimation(animation);
             }
         });
     }
